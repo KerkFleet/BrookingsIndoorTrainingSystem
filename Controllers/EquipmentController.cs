4eabbb118a62935f9ed2f523027e38d69f8081db
Nomeara <omeara.nate@gmail.com>
2021-04-11 19:42:55 -0500

    Got add eqipment connected to the database

---
+++ Controllers/EquipmentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrookingsIndoorTrainingSystem.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        // GET: Equipment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Getlist()
+        {
+
+            return View("EquipmentAddItemView");
+        }
+
+    }
+}
