d550ed80b9cba974996696e9a4dbb9118d15d3f8
Shelby Kerkvliet <kerkvliet21@gmail.com>
2021-04-13 13:55:10 -0500

    Added funds view

---
+++ Models/FundsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BrookingsIndoorTrainingSystem.Models
+{
+    public class FundsModel
+    {
+        public int id { get; set; }
+        public float Concessions { get; set; }
+        public float Equipment { get; set; }
+        public float Space { get; set; }
+        public float BITS { get; set; }
+
+    }
+}
